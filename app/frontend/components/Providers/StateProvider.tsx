220f5f6beb198782a6b4a8cbe41a6e1cb8b72eb9
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-05-07 11:43:01 -0500

    TODO: Refactor locations

--- app/frontend/components/Providers/StateProvider.tsx
+++ app/frontend/components/Providers/StateProvider.tsx
@@ -1,13 +1,25 @@
-import { createContext } from "use-context-selector";
+import React from "react";
+import { createContext, useContextSelector } from "use-context-selector";
 import { DocumentStudyDocument } from "@/utils/types";
 
 // STATE MANATEMENT
 interface AppState {
   documents: {
+    fetchAttempted: boolean;
+    apiError: string | null;
     areLoading: boolean;
     list: DocumentStudyDocument[];
   };
 }
+
+const INITIAL_APP_STATE: AppState = {
+  documents: {
+    fetchAttempted: false,
+    apiError: null,
+    areLoading: false,
+    list: [],
+  },
+};
 
 type AppAction = { type: "INITIATE_DOCUMENT_UPLOAD" };
 
@@ -15,4 +27,30 @@ export const reduce = (state: AppState, action: AppAction): AppState => {
   return state;
 };
 
-const DocumentStudyContext = createContext<AppState | null>(null);
+// REACT MANAGEMENT
+export const DocumentStudyContext = createContext<AppState>(INITIAL_APP_STATE);
+
+type Selector<T> = (appState: AppState) => T;
+
+export const useAppState = function <T>(selector: Selector<T>): T {
+  return useContextSelector(DocumentStudyContext, selector);
+};
+
+export const DispatchContext =
+  React.createContext<React.Dispatch<AppAction> | null>(null);
+
+interface StateProviderProps {
+  children: React.ReactNode;
+}
+
+export const StateProvider = (props: StateProviderProps) => {
+  const { children } = props;
+  const [state, dispatch] = React.useReducer(reduce, INITIAL_APP_STATE);
+  return (
+    <DispatchContext.Provider value={dispatch}>
+      <DocumentStudyContext.Provider value={state}>
+        {children}
+      </DocumentStudyContext.Provider>
+    </DispatchContext.Provider>
+  );
+};
