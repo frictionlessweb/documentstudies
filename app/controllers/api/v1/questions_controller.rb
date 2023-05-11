e2d087e6f66e1f0a9e00a765b7ab228b194fcce7
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-05-10 19:51:40 -0500

    Creating a free response question works

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -2,11 +2,17 @@ class Api::V1::QuestionsController < ApplicationController
   before_action :authenticate_admin!
 
   def create
+    create_params[:questions].each do |question_param|
+      klass = question_param[:question_type].delete(:kind)
+      question_type = klass.constantize.create!(question_param[:question_type])
+      Question.create!(name: question_param[:name], question_type: question_type)
+    end
     render json: {success: true}
   end
 
   private
 
   def create_params
+    params.permit(questions: [:name, question_type: [:kind, :text]])
   end
 end
--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -9,15 +9,19 @@ class DocumentsControllerTest < ActionDispatch::IntegrationTest
   end
   test 'A user can create a free response question' do
     sign_in Admin.find_by(name: 'Susan')
-    post api_v1_create_questions_path, params: [
-      {
-        name: 'First Question',
-        question_type: {
-          kind: 'FreeResponseQuestion',
-          text: 'My new text'
+    post api_v1_create_questions_path, params: {
+      questions: [
+        {
+          name: 'First Question',
+          question_type: {
+            kind: 'FreeResponseQuestion',
+            text: 'My new text'
+          }
         }
-      }
-    ], as: :json
+      ]
+    }, as: :json
     assert_response :success
+    assert Question.find_by(name: 'First Question')
+    assert FreeResponseQuestion.find_by(text: 'My new text')
   end
 end
