d5b81fcfa3559ef79547e2558db4dd4596306b66
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-05-20 14:43:23 -0500

    Test that next_assignment behaves as expected

--- app/models/study.rb
+++ app/models/study.rb
@@ -5,7 +5,7 @@ class Study < ApplicationRecord
   def next_assignment
     groups = schema['groups']
     last_assignment_group = StudyAssignment.select(:group).where(study: self).order('created_at DESC')&.first&.group
-    last_index = groups.find_index(last_assignment_group) || 0
+    last_index = groups.find_index(last_assignment_group) || -1
     group = groups[(last_index + 1) % groups.length]
     StudyAssignment.create!(study: self, group:, results: schema)
   end
