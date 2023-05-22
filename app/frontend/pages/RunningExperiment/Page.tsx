79eeaefa1be3f730f68b416f366ae97c8c479bce
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-05-22 18:55:28 -0500

    Render instructions

--- app/frontend/pages/RunningExperiment/Page.tsx
+++ app/frontend/pages/RunningExperiment/Page.tsx
@@ -4,6 +4,7 @@ import { TextLayout } from "@/pages/RunningExperiment/TextLayout";
 import { PdfLayout } from "@/pages/RunningExperiment/PdfLayout";
 import { Task } from "@/pages/RunningExperiment/Task";
 import { NextButton } from "@/components/NextButton";
+import { Flex, Heading } from "@adobe/react-spectrum";
 
 interface PageProps {
   page: PageV0;
@@ -18,11 +19,19 @@ export const Page = (props: PageProps) => {
   const { page } = props;
   const Layout = LAYOUT_MAP[page.page_layout];
   return (
-    <Layout page={page}>
-      {page.tasks.map((task, taskIndex) => {
-        return <Task key={task.id} taskIndex={taskIndex} taskType={task} />;
-      })}
-      <NextButton />
-    </Layout>
+    <Flex direction="column" alignItems="center">
+      <Heading level={3} marginBottom={0}>
+        INSTRUCTIONS
+      </Heading>
+      <Flex maxWidth="500px">
+        <div dangerouslySetInnerHTML={{ __html: page.instructions }} />
+      </Flex>
+      <Layout page={page}>
+        {page.tasks.map((task, taskIndex) => {
+          return <Task key={task.id} taskIndex={taskIndex} taskType={task} />;
+        })}
+        <NextButton />
+      </Layout>
+    </Flex>
   );
 };
