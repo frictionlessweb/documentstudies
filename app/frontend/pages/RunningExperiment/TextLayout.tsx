38c8f5559839812e3009c41e66998a49ee5cf6ce
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-05-22 19:17:03 -0500

    Fix maxWidth

--- app/frontend/pages/RunningExperiment/TextLayout.tsx
+++ app/frontend/pages/RunningExperiment/TextLayout.tsx
@@ -5,7 +5,7 @@ import { LayoutProps } from "@/pages/RunningExperiment/types";
 export const TextLayout = (props: LayoutProps) => {
   const { children } = props;
   return (
-    <Flex width="100%" direction="column" alignItems="center">
+    <Flex width="100%" maxWidth="600px" direction="column" alignItems="center">
       <Heading level={3}>TASKS</Heading>
       {children}
     </Flex>
