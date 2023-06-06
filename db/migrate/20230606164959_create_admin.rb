85ecbe3a61d0ef9b1d082a4055486cd0912b8419
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-06-06 12:10:29 -0500

    Fix passwords

---
+++ db/migrate/20230606164959_create_admin.rb
@@ -0,0 +1,11 @@
+class CreateAdmin < ActiveRecord::Migration[7.0]
+  def up
+    password = ENV['ADOBE_PRODUCTION_PASSWORD']
+    Admin.create!(name: 'Adobe Internal', email: 'asiu@adobe.com',
+                  password:) 
+  end
+
+  def down
+    Admin.find_by(name: 'Adobe Internal').destroy!
+  end
+end
