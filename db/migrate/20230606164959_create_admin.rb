7efdf8267e9300196c01f38be194988d11715da6
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-06-06 12:17:36 -0500

    Adds password

--- db/migrate/20230606164959_create_admin.rb
+++ db/migrate/20230606164959_create_admin.rb
@@ -1,7 +1,7 @@
 class CreateAdmin < ActiveRecord::Migration[7.0]
   def up
     password = ENV['ADOBE_PRODUCTION_PASSWORD']
-    Admin.create!(name: 'Adobe Internal', email: 'asiu@adobe.com',
+    Admin.create!(name: 'Adobe Internal', email: 'production@adobe.com',
                   password:)
   end
 
